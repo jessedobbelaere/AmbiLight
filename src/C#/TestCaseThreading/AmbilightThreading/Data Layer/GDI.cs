4bdabea21665e391bef63d935d991263651a3993
Michael Lucas <michael.lucas@kahosl.be>
2012-04-28 11:40:10 +0200

    ScreencapThreaded.cs aangepast naar performantere versie met GDI
    Signed-off-by: Michael Lucas <michael.lucas@kahosl.be>

---
+++ src/C#/TestCaseThreading/AmbilightThreading/Data Layer/GDI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace AmbilightThreading.Data_Layer {
+    class GDI {
+
+        [DllImport("gdi32.dll")]
+        public static extern bool BitBlt(
+          IntPtr hdcDest, // handle to destination DC
+          int nXDest, // x-coord of destination upper-left corner
+          int nYDest, // y-coord of destination upper-left corner
+          int nWidth, // width of destination rectangle
+          int nHeight, // height of destination rectangle
+          IntPtr hdcSrc, // handle to source DC
+          int nXSrc, // x-coordinate of source upper-left corner
+          int nYSrc, // y-coordinate of source upper-left corner
+          System.Int32 dwRop // raster operation code
+          );
+
+        [DllImport("User32.dll")]
+        public extern static System.IntPtr GetDC(System.IntPtr hWnd);
+        [DllImport("User32.dll")]
+        public extern static int ReleaseDC(System.IntPtr hWnd, System.IntPtr hDC);
+    }
+}
--- src/C#/TestCaseThreading/AmbilightThreading/Overkoepelend/ColorSources/ScreencapThreaded.cs
+++ src/C#/TestCaseThreading/AmbilightThreading/Overkoepelend/ColorSources/ScreencapThreaded.cs
@@ -7,7 +7,8 @@ using System.Timers;
 using System.Windows.Forms;
 using System.Drawing.Imaging;
 using System.Threading;
-using AForge.Imaging.ColorReduction;
+using AmbilightThreading.Data_Layer;
+// AForge.Imaging.ColorReduction;
 
 namespace TestCaseThreading.ColorSources {
     class ScreencapThread : ColorSource {
@@ -21,40 +22,59 @@ namespace TestCaseThreading.ColorSources {
         public ScreencapThread(SerialCom sc) {
             this.serial = sc;
             this.size = Screen.PrimaryScreen.Bounds.Size;
+            t = new Thread(new ThreadStart(capScreen));
+            t.Name = "Screen capture and analysation thread";
+            t.IsBackground = true;
         }
 
         public void Start() {
             this.running = true;
-            t = new Thread(new ThreadStart(capScreen));
-            t.Name = "Screen capture and analysation thread";
-            t.IsBackground = true;
             t.Start();
         }
         public void Stop() {
             this.running = false;
             t.Abort();
+
             Output(15, 0, 0, 0); //alle leds uit
         }
 
         // vanaf hier screencapture specifieke code
         private void capScreen() {
+            int teller=0;
             while (running) {
+                teller++;
                 Bitmap bmp;
-                Graphics gfx;
+                Graphics g;
                 try {
+                    System.IntPtr desktopDC = GDI.GetDC(System.IntPtr.Zero);
+                    bmp = new Bitmap(SystemInformation.VirtualScreen.Width, SystemInformation.VirtualScreen.Height);
+                    g = Graphics.FromImage(bmp);
+                    System.IntPtr bmDC = g.GetHdc();
+
+                    GDI.BitBlt(bmDC, 0, 0, bmp.Width, bmp.Height, desktopDC, 0, 0, 0x00CC0020 /*SRCCOPY*/);
+
+                    GDI.ReleaseDC(System.IntPtr.Zero, desktopDC);
+                    g.ReleaseHdc(bmDC);
+                    g.Dispose();
+
+                    /*
                     //aanmaken objecten voor opslag/verwerken afbeelding
                     bmp = new Bitmap(size.Width,size.Height, PixelFormat.Format24bppRgb);
                     gfx = Graphics.FromImage(bmp);
 
                     //screencap+save
                     gfx.CopyFromScreen(0, 0, 0, 0, size, CopyPixelOperation.SourceCopy);
-
+                    */
                 }
                 catch (ArgumentException error) {
                     System.Diagnostics.Debug.Print(error.ToString());
                     return;
                 }
 
+                //debugdebugdebugdebug
+                if (teller % 30 == 0) {
+                    bmp.Save("test" + teller.ToString() + ".bmp");
+                }
                 BitmapData srcData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height),ImageLockMode.ReadOnly,PixelFormat.Format24bppRgb);
 
                 int stride = srcData.Stride;
@@ -84,28 +104,11 @@ namespace TestCaseThreading.ColorSources {
 
                 Output(0, avgR, avgG, avgB);
 
-                /* Veel trager(1.5 FPS vs 5 FPS op deze laptop bij 1920*1080) maar waarschijnlijk correcter algoritme. Source daar eens van bekijken?
-                 
-                MedianCutQuantizer algor=new MedianCutQuantizer();
-                    unsafe {
-                                    byte* p = (byte*)(void*)Scan0;
-
-                                    for (int y = 0; y < height; y++) {
-                                        for (int x = 0; x < width; x++) {
-                                            int i = (y * stride) + x * 3;
-
-                                            algor.AddColor(Color.FromArgb(p[i],p[i+1],p[i+2]));
-                                            }
-                                        }
-                                    }
-                
-
-                                Color[] kleur=algor.GetPalette(1);
-                                Output(0, kleur[0].R, kleur[0].G, kleur[0].B);
-                */
                 bmp.UnlockBits(srcData);//opruimen van rommel hier
                 bmp.Dispose();
-                gfx.Dispose();
+                bmp = null;
+                g.Dispose();
+                g = null;
                 GC.Collect(); 
             }
 
